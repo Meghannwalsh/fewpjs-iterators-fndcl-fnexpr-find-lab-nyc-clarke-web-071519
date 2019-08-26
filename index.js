ffba4fdf4c2aabf7d0527c696fa2eb8e1f61c93b
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-08-26 13:47:12 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+// function superbowlWin(array){
+//   find(i = 0; i < array.length; i +=1){
+//     superbowl = array[i]
+    
+//   }
+// }
+
+function superbowlWin(array) {
+  const arr = array.find(function (win) {
+   return win.result === "W"
+  })
+  if (arr){
+    console.log(arr)
+    return arr.year
+  } else {
+    return arr
+  }
+
+}
+
+
+
+// roommates.find(function (s) { return s === "winston" })
